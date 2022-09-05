41db0f296e7e7c22775c4b4af8fa7a22f66cd059
jpoh281 <hjp4869@naver.com>
2022-09-05 23:12:18 +0900

    뷰모델 및 프레그먼트 로그 찍어보기

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,8 +1,19 @@
 package com.example.android.unscramble.ui.game
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
+
+    init {
+        Log.d("GameFragment", "GameViewModel created!")
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.d("GameFragment", "GameViewModel destroyed!")
+    }
+
     private var score = 0
     private var currentWordCount = 0
 
